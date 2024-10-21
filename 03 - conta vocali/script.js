ff62571f558e4b0fa546b0e716e44ada51d1f830
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-21 15:28:05 +0200

    es 4 add function

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
+const vowels = ["a", "e", "i", "o", "u", "y"];
 
 // Dichiara la funzione qui.
 
+function howManyVowels(string) {
+  let count = 0;
+  let wichVowels = "";
+  for (let i = 0; i < vowels.length; i++) {
+    if (string.includes(vowels[i])) {
+      count++;
+      wichVowels += " " + vowels[i];
+    }
+  }
+  return count + " " + wichVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const vowelsCount = howManyVowels(word);
+console.log(vowelsCount);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)

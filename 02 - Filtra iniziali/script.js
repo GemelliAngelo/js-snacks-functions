5ae4e58c659b37ade6fe034503b3bbd4f7da1845
GemelliAngelo <gemeangelo27@gmail.com>
2024-10-21 14:56:23 +0200

    es 2 complete

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,24 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+let sameInitials = [];
+const namesInitialChar = "L";
 
 // Dichiara la funzione qui.
 
+function getSameInitials(array, sameInitialsArray, initialChar) {
+  for (let i = 0; i < array.length; i++) {
+    const string = array[i];
+    if (string.charAt(0) === initialChar) {
+      sameInitialsArray.push(string);
+    }
+  }
+  return sameInitialsArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+sameInitials = getSameInitials(names, sameInitials, namesInitialChar);
+console.log(sameInitials);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
